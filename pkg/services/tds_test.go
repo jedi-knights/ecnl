ee0808991444b201f31aa06a8ad153f34a448a7c
Omar Crosby <omar.crosby@gmail.com>
2023-10-01 15:24:42 -0400

    chore(translations): adding a single test

---
+++ pkg/services/tds_test.go
@@ -0,0 +1,12 @@
+package services_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Tds", func() {
+	It("blue sky sunny day", func() {
+		Expect(true).To(BeTrue())
+	})
+})
