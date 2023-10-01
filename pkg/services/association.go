4de92b77bc83fd8df0c1e9cfbda8e546c1491ea2
Omar Crosby <omar.crosby@gmail.com>
2023-10-01 11:39:31 -0400

    chore: removing GetAllCountries

--- pkg/services/association.go
+++ pkg/services/association.go
@@ -39,45 +39,50 @@ func (s *AssociationService) GetUrl(path string) (string, error) {
 	return targetUrl, nil
 }
 
-func (s *AssociationService) GetAllCountries() ([]models.Country, error) {
-	var err error
-	var data []byte
-	var targetUrl string
-	var pResponse *http.Response
-	var output struct {
-		Result    string           `json:"result"`
-		Countries []models.Country `json:"data"`
-	}
-
-	if targetUrl, err = s.GetUrl("get-all-countries"); err != nil {
-		return nil, err
-	}
-
-	if pResponse, err = s.HttpClient.Get(targetUrl); err != nil {
-		return nil, fmt.Errorf("Error getting countries: %v\n", err)
-	}
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-			fmt.Println(err.Error())
-			panic("an error occured while attempting to close the body")
-		}
-	}(pResponse.Body)
-
-	if pResponse.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("Invalid status code %d: ", pResponse.StatusCode)
-	}
-
-	if data, err = io.ReadAll(pResponse.Body); err != nil {
-		return nil, fmt.Errorf("Error reading response body: %v\n", err)
-	}
-
-	if err = json.Unmarshal(data, &output); err != nil {
-		return nil, fmt.Errorf("Error unmarshalling response body: %v\n", err)
-	}
-
-	return output.Countries, nil
-}
+//func (s *AssociationService) GetAllCountries() ([]models.Country, error) {
+//	var err error
+//	var statusCode int
+//	var data []byte
+//	var targetUrl string
+//	var pResponse *http.Response
+//	var output struct {
+//		Result    string           `json:"result"`
+//		Countries []models.Country `json:"data"`
+//	}
+//
+//	if targetUrl, err = s.GetUrl("get-all-countries"); err != nil {
+//		return nil, err
+//	}
+//
+//	if statusCode, err = json2.NewClient().Get(targetUrl, &output); err != nil {
+//
+//	}
+//
+//	if pResponse, err = s.HttpClient.Get(targetUrl); err != nil {
+//		return nil, fmt.Errorf("Error getting countries: %v\n", err)
+//	}
+//	defer func(Body io.ReadCloser) {
+//		err := Body.Close()
+//		if err != nil {
+//			fmt.Println(err.Error())
+//			panic("an error occured while attempting to close the body")
+//		}
+//	}(pResponse.Body)
+//
+//	if pResponse.StatusCode != http.StatusOK {
+//		return nil, fmt.Errorf("Invalid status code %d: ", pResponse.StatusCode)
+//	}
+//
+//	if data, err = io.ReadAll(pResponse.Body); err != nil {
+//		return nil, fmt.Errorf("Error reading response body: %v\n", err)
+//	}
+//
+//	if err = json.Unmarshal(data, &output); err != nil {
+//		return nil, fmt.Errorf("Error unmarshalling response body: %v\n", err)
+//	}
+//
+//	return output.Countries, nil
+//}
 
 func (s *AssociationService) GetAllStates() ([]models.State, error) {
 	var err error
